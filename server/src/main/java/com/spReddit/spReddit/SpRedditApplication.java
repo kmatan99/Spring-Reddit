d662547951ed4f2dc6b3d850f7772b8b607c4586
Karlo <kmatan01@fesb.hr>
2020-12-15 15:27:17 +0100

    Creating project client and server side

---
+++ server/src/main/java/com/spReddit/spReddit/SpRedditApplication.java
@@ -0,0 +1,13 @@
+package com.spReddit.spReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpRedditApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/spReddit/spReddit/SpRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spReddit.spReddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
