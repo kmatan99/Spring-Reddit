33842f22bdeace2121e3119070edb3e2154c916f
Karlo <kmatan01@fesb.hr>
2021-01-09 16:49:36 +0100

    Add JWT Models for auth

--- server/src/main/java/com/spReddit/spReddit/repository/UserRepository.java
+++ server/src/main/java/com/spReddit/spReddit/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     public Optional<UserEntity> findById(Long id);
+    public Optional<UserEntity> findByUsername(String username);
 }
