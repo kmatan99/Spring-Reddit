4e97547cdd441e7b29a8bf380d144911ddb52dea
Karlo <kmatan01@fesb.hr>
2021-01-09 16:49:59 +0100

    Add auth service and util

---
+++ server/src/main/java/com/spReddit/spReddit/service/JwtUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.spReddit.spReddit.service;
+
+import com.spReddit.spReddit.model.UserEntity;
+import com.spReddit.spReddit.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+
+@Service
+@AllArgsConstructor
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        UserEntity userEntity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        return new User(userEntity.getUsername(), userEntity.getPassword(), new ArrayList<>());
+    }
+}
